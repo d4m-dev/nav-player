45ecf043ff2af300133fd0d940529e5616376ac1
Ly Thua An <andurex5555@gmail.com>
2025-07-02 19:28:12 +0700

    Add files via upload

--- tracks.js
+++ tracks.js
@@ -4,6 +4,7 @@ const tracks = [
     artist: "Hoàng Tiêu Vân | Trường Nguyệt Tẫn Minh OST",
     artwork: "https://i.postimg.cc/sfB4vtbq/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/cunhuvaymotvannam/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/cunhuvaymotvannam/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/cunhuvaymotvannam/1.lrc",
   },
   {
@@ -11,6 +12,7 @@ const tracks = [
     artist: "ĐỨC PHÚC x 911 x KHẮC HƯNG  OFFICIAL",
     artwork: "https://i.postimg.cc/TYDMgYZM/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/weddingsongs/ido.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/weddingsongs/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/weddingsongs/1.lrc",
   },
   {
@@ -18,6 +20,7 @@ const tracks = [
     artist: "Hồ Phong An x HuyN FT",
     artwork: "https://i.postimg.cc/SQCRjdNk/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/vokichcuaem/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/vokichcuaem/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/vokichcuaem/1.lrc",
   },
   {
@@ -25,6 +28,7 @@ const tracks = [
     artist: "DC Tâm x SS x AM Remix",
     artwork: "https://i.postimg.cc/8PTpcV0m/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/yeuemnhungkhongvoitoi/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/yeuemnhungkhongvoitoi/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/yeuemnhungkhongvoitoi/1.lrc",
   },
   {
@@ -32,6 +36,7 @@ const tracks = [
     artist: "Trần Mạnh Cường",
     artwork: "https://i.postimg.cc/5t3nSLmr/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/anhdautulucemdi/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/anhdautulucemdi/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/anhdautulucemdi/1.lrc",
   },
   {
@@ -39,6 +44,7 @@ const tracks = [
     artist: "Linh Hương Luz",
     artwork: "https://i.postimg.cc/B62DC0sn/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/manhba/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/manhba/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/manhba/1.lrc",
   },
   {
@@ -46,6 +52,7 @@ const tracks = [
     artist: "Hoàng Oanh x ACV",
     artwork: "https://i.postimg.cc/GhwgrL9q/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/diadang/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/diadang/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/diadang/1.lrc",
   },
   {
@@ -53,6 +60,7 @@ const tracks = [
     artist: "Tùng Dương x ACV",
     artwork: "https://i.postimg.cc/mk0vWCJw/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/taisinh/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/taisinh/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/taisinh/1.lrc",
   },
   {
@@ -60,6 +68,7 @@ const tracks = [
     artist: "Cần Vinh x Lee Ken",
     artwork: "https://i.postimg.cc/7hLc2m8X/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/aihongnhan/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/aihongnhan/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/aihongnhan/1.lrc",
   },
   {
@@ -67,6 +76,7 @@ const tracks = [
     artist: "Jank",
     artwork: "https://i.postimg.cc/v8Wk4gJp/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/thuongthithoi/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/thuongthithoi/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/thuongthithoi/1.lrc",
   },
   {
@@ -74,6 +84,7 @@ const tracks = [
     artist: "Lâm Tuấn x MewMew lofi",
     artwork: "https://i.postimg.cc/K85HJ6GQ/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/bakieptinhmotkiepduyen/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/bakieptinhmotkiepduyen/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/bakieptinhmotkiepduyen/1.lrc",
   },
   {
@@ -81,6 +92,7 @@ const tracks = [
     artist: "Mochiii x Domino Remix",
     artwork: "https://i.postimg.cc/4d3yYRd0/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/tralaithanhxuanchoem/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/tralaithanhxuanchoem/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/tralaithanhxuanchoem/1.lrc",
   },
   {
@@ -88,6 +100,7 @@ const tracks = [
     artist: "旺仔小乔",
     artwork: "https://i.postimg.cc/htC0JcPF/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/daohoanac/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/daohoanac/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/daohoanac/1.lrc",
   },
   {
@@ -95,6 +108,7 @@ const tracks = [
     artist: "Vương Tĩnh Văn",
     artwork: "https://i.postimg.cc/wx1byWs1/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/vaygiu/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/vaygiu/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/vaygiu/1.lrc",
   },
   {
@@ -102,6 +116,7 @@ const tracks = [
     artist: "Anh Tú",
     artwork: "https://i.postimg.cc/3NMRVV3b/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/khoalybiet/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/khoalybiet/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/khoalybiet/1.lrc",
   },
   {
@@ -109,6 +124,7 @@ const tracks = [
     artist: "Linh Hương Luz",
     artwork: "https://i.postimg.cc/05cDXG3G/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/anhthoinhannhuong/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/anhthoinhannhuong/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/anhthoinhannhuong/1.lrc",
   },
   {
@@ -116,6 +132,7 @@ const tracks = [
     artist: "Chu Bin",
     artwork: "https://i.postimg.cc/tJzQQ37W/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/noidautimthayem/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/noidautimthayem/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/noidautimthayem/1.lrc",
   },
   {
@@ -123,6 +140,7 @@ const tracks = [
     artist: "Anh Quân x Đông Thiên Đức",
     artwork: "https://i.postimg.cc/tJ8PbxfR/1.jpg",
     path: "https://github.com/d4m-dev/media/raw/main/elakhongthe/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/elakhongthe/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/elakhongthe/1.lrc",
   },
   {
@@ -137,6 +155,7 @@ const tracks = [
     artist: "Dickson x Thành Đạt",
     artwork: "https://i.postimg.cc/MHZ67gf9/1.jpg",
     path: "https://raw.githubusercontent.com/d4m-dev/media/main/cantinhnhuthe/2.mp3",
+    instrumental: "https://github.com/d4m-dev/media/raw/main/cantinhnhuthe/3.mp3",
     lyric: "https://raw.githubusercontent.com/d4m-dev/media/main/cantinhnhuthe/1.lrc",
   },
 ];
